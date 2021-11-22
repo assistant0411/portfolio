996e13c3a050cee7e6f693d68710248b7d594ffe
DomDev126 <dom.wolf126@gmail.com>
2021-11-23 07:17:03 +0800

    small fixes

---
+++ components/icons.js
@@ -0,0 +1,155 @@
+/* eslint-disable */
+import React from 'react'
+
+
+export const Github = () => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-github"
+      viewBox="0 0 24 24"
+    >
+      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"></path>
+    </svg>
+    )
+}
+
+
+export const LinkedIn = () => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="feather feather-linkedin"
+        viewBox="0 0 24 24"
+      >
+        <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6z"></path>
+        <path d="M2 9H6V21H2z"></path>
+        <circle cx="4" cy="4" r="2"></circle>
+      </svg>
+    )
+}
+
+export const Dribbble =() => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-dribbble"
+      viewBox="0 0 24 24"
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <path d="M8.56 2.75c4.37 6.03 6.02 9.42 8.03 17.72m2.54-15.38c-3.72 4.35-8.94 5.66-16.88 5.85m19.5 1.9c-3.5-.93-6.63-.82-8.94 0-2.58.92-5.01 2.86-7.44 6.32"></path>
+    </svg>
+    )
+}
+
+
+export const Coffee =() => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-coffee"
+      viewBox="0 0 24 24"
+    >
+      <path d="M18 8h1a4 4 0 010 8h-1M2 8h16v9a4 4 0 01-4 4H6a4 4 0 01-4-4V8z"></path>
+      <path d="M6 1L6 4"></path>
+      <path d="M10 1L10 4"></path>
+      <path d="M14 1L14 4"></path>
+    </svg>
+    )
+}
+
+
+export const Twitter =() => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="feather feather-twitter"
+        viewBox="0 0 24 24"
+      >
+        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+      </svg>
+    )
+}
+
+
+export const Facebook =() => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="feather feather-facebook"
+        viewBox="0 0 24 24"
+      >
+        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+      </svg>
+    )
+}
+
+
+export const Email =() => {
+    return(
+        <svg
+        style={{color: '#0070f3'}}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-mail"
+      viewBox="0 0 24 24"
+    >
+      <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+      <path d="M22 6L12 13 2 6"></path>
+    </svg>
+    )
+}
