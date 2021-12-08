947567d46294b20788e358c046e6cfcd5087bfde
DomDev126 <dom.wolf126@gmail.com>
2021-12-08 21:43:38 +0800

    removed buymeacoffee

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Panshak Solomon - Full stack Developer (React/Nodejs) </title>
         <meta name="description" content="Panshak Solomon - Conservation Biologist / Software Developer" />
         <link rel="icon" href="/favicon.ico" />
-        <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="panshak" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+        {/* <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="panshak" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script> */}
       </Head>
 
       <main className={styles.main}>
