25cca8b8cede68c65e4c31e3f3465be88b38a5d7
DomDev126 <dom.wolf126@gmail.com>
2023-04-18 22:11:52 +0800

    minor edit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Panshak Solomon - Conservation Biologist / Software Developer </title>
+        <title>Panshak Solomon - Conservation | Urban Ecology | Citizen Science </title>
         <meta name="description" content="Panshak Solomon - Conservation Biologist / Software Developer" />
         <link rel="icon" href="/favicon.ico" />
         {/* <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="panshak" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script> */}
@@ -18,17 +18,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-       Hello,
+       Panshak S.K.
         </h1>
 
         <p className={styles.description}>
-        I am Panshak Solomon, a Conservation Biologist and a Software Developer
-        with proven experience developing and managing software applications for biodiversity conservation.
-        I love field work and I'm actively involve in conservation research. 
+        My resaerch interests focus mainly on urban ecology and citizen science. I have been conducting field surveys, analyzing large datasets, and utilizing GIS and statistical software to contribute to the scientific understanding of urban ecosystems. 
        <span>
-       <a style={{color: '#0070F3', paddingRight: '8px'}} href="https://doi.org/10.1016/j.ufug.2021.127092" target="_blank"> Read my recent publication here.</a>
+       <a style={{color: '#0070F3', paddingRight: '8px'}} href="https://doi.org/10.1016/j.ufug.2021.127092" target="_blank"> You can read my recent publication here.</a>
        </span>
-       I currently work with the A.P. Leventis Ornithological Research institute, University of Jos Biological Conservatory.
+       I currently work with the A.P. Leventis Ornithological Research institute at the University of Jos Biological Conservatory.
+        In my free time, I love to build tools for biodiversity conservation.
         </p>
        
        <div className={styles.avatar}>
@@ -47,11 +46,11 @@ export default function Home() {
       <div className={styles.socials}>
           <ul className={styles.autoGrid}>
 
-          <a target="_blank" href="https://github.com/Panshak" rel="noopener noreferrer">
+          {/* <a target="_blank" href="https://github.com/Panshak" rel="noopener noreferrer">
               <li className={styles.listItem}>
                 <Github />
               </li>
-            </a>
+            </a> */}
 
             <a target="_blank" href="mailto:ipanshak@gmail.com" rel="noopener noreferrer">
               <li className={styles.listItem}>
@@ -65,11 +64,11 @@ export default function Home() {
               </li>
             </a>
 
-            <a target="_blank" href="https://dribbble.com/panshak" rel="noopener noreferrer">
+            {/* <a target="_blank" href="https://dribbble.com/panshak" rel="noopener noreferrer">
               <li className={styles.listItem}>
                 <Dribbble />
               </li>
-            </a>
+            </a> */}
 
 
             <a target="_blank" href="https://twitter.com/panshak_" rel="noopener noreferrer">
